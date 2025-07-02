1ac50422fb1579a8958a1d7bf70eca5e1b728f53
Marko Kvakić <mkvakic@dhz.hr>
2025-07-02 08:46:27 +0200

    face.sh update

--- face.sh
+++ face.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Get image paths as an array
-image_paths=($(find UFDD_val/images/snow/*jpg -not -wholename '*labelled*'))
+image_paths=($(find UFDD_val/images -type f -and -not -wholename '*labelled*'))
 
 # Run the detection algorithm on each of them
-for image_path in ${image_paths[@]:0:30};
+for image_path in ${image_paths[@]};
 do
     ./face.sif ${image_path}
 done
